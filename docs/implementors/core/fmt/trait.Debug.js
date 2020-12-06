fa25686c87b2dd3aae1be41d346929725b398a2a
djedou <djedouarnaud.aman@gmail.com>
2020-12-06 11:57:29 +0100

    doc

---
+++ docs/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["kongodjan"] = [{"text":"impl Debug for FullConnectedLayer","synthetic":false,"types":[]},{"text":"impl&lt;L:&nbsp;Debug&gt; Debug for Network&lt;L&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;L: LayerT + Clone,&nbsp;</span>","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ docs/implementors/std/panic/trait.UnwindSafe.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["kongodjan"] = [{"text":"impl UnwindSafe for FullConnectedLayer","synthetic":true,"types":[]},{"text":"impl&lt;L&gt; UnwindSafe for Network&lt;L&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;L: UnwindSafe,&nbsp;</span>","synthetic":true,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
